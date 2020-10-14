db9d6ed45d5cc5b5ac3782eeb71640ec286ff11c
reeshmn <40357592+reeshmn@users.noreply.github.com>
2020-10-14 17:07:48 +0530

    Update MainActivity.java

--- app/src/main/java/com/skytailindia/popmusicplayer/MainActivity.java
+++ app/src/main/java/com/skytailindia/popmusicplayer/MainActivity.java
@@ -14,14 +14,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent intent = new Intent(MainActivity.this, PopMain.class);
-                startActivity(intent);
-                finish();
-            }
-        },SPLASH_SCREEN);
+     
 
 
     }
